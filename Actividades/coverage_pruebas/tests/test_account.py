4efd05d6d78ae95eaf94bf4bbd2f7f783a93df1b
Juan Silva <juan.silva.r@uni.pe>
2024-10-23 12:25:58 +0000

    Avance actividad

--- Actividades/coverage_pruebas/tests/test_account.py
+++ Actividades/coverage_pruebas/tests/test_account.py
@@ -4,7 +4,8 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from models import db
-from models.account import Account
+from models.account import Account, DataValidationError
+import random
 
 ACCOUNT_DATA = {}
 
@@ -59,3 +60,66 @@ class TestAccountModel:
             account = Account(**data)
             account.create()
         assert len(Account.all()) == len(ACCOUNT_DATA)
+
+
+### GETTING A 100% OF COVERAGE
+def test_repr():
+    """Prueba la representación de una cuenta"""
+    account = Account()
+    account.name = "Foo"
+    assert str(account) == "<Account 'Foo'>"
+
+def test_to_dict():
+    """Prueba la conversión de una cuenta a diccionario"""
+    random_key1=random.randint(1,5)
+    data = ACCOUNT_DATA[random_key1]  # obtener una cuenta aleatoria
+    account = Account(**data)
+    result = account.to_dict()
+    print(result)
+    assert account.name == result["name"]
+    assert account.email == result["email"]
+    assert account.phone_number == result["phone_number"]
+    assert account.disabled == result["disabled"]
+    assert account.date_joined == result["date_joined"]
+
+def test_from_dict():
+    """Prueba establecer atributos de una cuenta desde un diccionario"""
+    random_key2=random.randint(1,5)
+    data = ACCOUNT_DATA[random_key2]  # obtener una cuenta aleatoria
+    account = Account()
+    account.from_dict(data)
+    assert account.name == data["name"]
+    assert account.email == data["email"]
+    assert account.phone_number == data["phone_number"]
+    assert account.disabled == data["disabled"]
+
+def test_update_account():
+    """Prueba la actualización de una cuenta utilizando datos conocidos"""
+    random_key3=random.randint(1,5)
+    data = ACCOUNT_DATA[random_key3]  # obtener una cuenta aleatoria
+    account = Account(**data)
+    account.create()
+    assert account.id is not None
+    account.name = "Rumpelstiltskin"
+    account.update()
+    found = Account.find(account.id)
+    assert found.name == account.name
+
+def test_update_invalid_id():
+    """Prueba la actualización de una cuenta con ID inválido"""
+    random_key4=random.randint(1,5)
+    data = ACCOUNT_DATA[random_key4]  # obtener una cuenta aleatoria
+    account = Account(**data)
+    account.id = None
+    with pytest.raises(DataValidationError):
+        account.update()
+
+def test_delete_account():
+    """Prueba la eliminación de una cuenta utilizando datos conocidos"""
+    random_key5=random.randint(1,5)
+    data = ACCOUNT_DATA[random_key5]  # obtener una cuenta aleatoria
+    account = Account(**data)
+    account.create()
+    assert len(Account.all()) == 1
+    account.delete()
+    assert len(Account.all()) == 0
